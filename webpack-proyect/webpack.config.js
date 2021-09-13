18fcd74d949abebbe0e73905e9d81f216aff6df8
chehin <chehinjuan@gmail.com>
2021-09-13 09:47:05 -0300

    Configuracion para vendor y bundle
    Se configuro webpack para que bundle sea lo mas pequeño y vendor posea las dependencias que no se actualizan con frecuencia

--- webpack-proyect/webpack.config.js
+++ webpack-proyect/webpack.config.js
@@ -1,11 +1,28 @@
 var webpack = require('webpack');
 var path = require('path');
 
+// Posee las librerias que voy a actualizar con baja frecuencia
+const VENDOR_LIBS = [
+    "faker",
+    "lodash",
+    "react",
+    "react-dom",
+    "react-input-range",
+    "react-redux",
+    "react-router",
+    "redux",
+    "redux-form",
+    "redux-thunk"
+]
+
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        bundle: './src/index.js', // Posee JS de nuestra app principal
+        vendor: VENDOR_LIBS // Posee las dependecias de nuestro proyecto
+    },
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: '[name].js'
     },
     module: {
         rules: [{
@@ -18,5 +35,10 @@ module.exports = {
                 test: /\.css$/
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor'
+        })
+    ]
 };
